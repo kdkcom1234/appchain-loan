7b65e0d6c6347f15b6e1bfb260be72a2ca2ef045
Daekeun Ko <kdkcom1234@gmail.com>
2024-05-08 14:08:10 +0900

    add-genesis-acount 명령어 추가

--- cmd/loand/cmd/commands.go
+++ cmd/loand/cmd/commands.go
@@ -42,6 +42,7 @@ func initRootCmd(
 	server.AddCommands(rootCmd, app.DefaultNodeHome, newApp, appExport, addModuleInitFlags)
 
 	validateGenesisCmd := genutilcli.ValidateGenesisCmd(basicManager) // <- add this line
+	addGenesisAccountCmd := genutilcli.AddGenesisAccountCmd(app.DefaultNodeHome, txConfig.SigningContext().AddressCodec())
 
 	// add keybase, auxiliary RPC, query, genesis, and tx child commands
 	rootCmd.AddCommand(
@@ -52,6 +53,7 @@ func initRootCmd(
 		keys.Commands(),
 
 		validateGenesisCmd, // <- Add this line
+		addGenesisAccountCmd,
 	)
 }
 
